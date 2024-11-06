8b09a524c6cd5452469dd461e16767e62a284d27
shirinze <sherize1373@gmail.com>
2024-11-06 17:23:15 +0300

    Testimonial, Services ve Car list Controlleri Eklendi ve dinamik hala getirildi

---
+++ Frontends/CarBook.WebUI/Controllers/ContactController.cs
@@ -0,0 +1,36 @@
+﻿using CarBook.Dto.ContactDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace CarBook.WebUI.Controllers
+{
+	public class ContactController : Controller
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+        public ContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+		[HttpGet]
+        public IActionResult Index()
+		{
+			return View();
+		}
+		[HttpPost]
+		public async Task<IActionResult> Index(CreateContactDto createcontactdto)
+		{
+			var client = _httpClientFactory.CreateClient();
+			createcontactdto.SendDate= DateTime.Now;
+			var jsonData = JsonConvert.SerializeObject(createcontactdto);
+			StringContent stringcontent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+			var responseMessage = await client.PostAsync("https://localhost:7100/api/Contacts", stringcontent);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index", "Default");
+			}
+			return View();
+		}
+	}
+}
