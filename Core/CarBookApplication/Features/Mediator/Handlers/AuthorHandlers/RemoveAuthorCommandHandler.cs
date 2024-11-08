66ece3d46c7e7aff48a3111cb92dc570396c0c36
shirinze <sherize1373@gmail.com>
2024-11-08 14:56:35 +0300

    Blog ve Author tablolari eklendi ve mediator ile api kontrolleri yazildi

---
+++ Core/CarBookApplication/Features/Mediator/Handlers/AuthorHandlers/RemoveAuthorCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using CarBook.Domain.Entities;
+using CarBookApplication.Features.Mediator.Commands.AuthorCommands;
+using CarBookApplication.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBookApplication.Features.Mediator.Handlers.AuthorHandlers
+{
+    public class RemoveAuthorCommandHandler : IRequestHandler<RemoveAuthorCommand>
+    {
+        private readonly IRepository<Author> _repository;
+
+        public RemoveAuthorCommandHandler(IRepository<Author> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(RemoveAuthorCommand request, CancellationToken cancellationToken)
+        {
+            var value = await _repository.GetByIDAsync(request.Id);
+            await _repository.RemoveAsync(value);
+        }
+    }
+}
---
+++ Core/CarBookApplication/Features/Mediator/Results/BlogResults/GetBlogByIdQueryResult.cs
@@ -0,0 +1,21 @@
+﻿using CarBook.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBookApplication.Features.Mediator.Results.BlogResults
+{
+    public class GetBlogByIdQueryResult
+    {
+        public int BlogID { get; set; }
+        public string Title { get; set; }
+        public string AuthorID { get; set; }
+        public Author Author { get; set; }
+        public string CoverImageUrl { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+    }
+}
