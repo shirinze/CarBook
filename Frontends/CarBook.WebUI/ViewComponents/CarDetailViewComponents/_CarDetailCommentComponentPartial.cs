e0c56aec12a5094d5a99b92484e779f10450f0e0
shirinze <sherize1373@gmail.com>
2025-01-04 15:40:37 +0300

    description ve review bolumu eklendi

---
+++ Frontends/CarBook.WebUI/ViewComponents/CarDetailViewComponents/_CarDetailCommentComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebUI.ViewComponents.CarDetailViewComponents
+{
+	public class _CarDetailCommentComponentPartial:ViewComponent
+	{
+		public IViewComponentResult Invoke(int id)
+		{
+			ViewBag.carid = id;
+			return View();
+		}
+	}
+}
--- Frontends/CarBook.WebUI/ViewComponents/CarDetailViewComponents/_CarDetailDescriptionComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/CarDetailViewComponents/_CarDetailDescriptionComponentPartial.cs
@@ -1,12 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.CarDescriptionDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBook.WebUI.ViewComponents.CarDetailViewComponents
 {
 	public class _CarDetailDescriptionComponentPartial:ViewComponent
 	{
-		public IViewComponentResult Invoke(int id)
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public _CarDetailDescriptionComponentPartial(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync(int id)
 		{
 			ViewBag.carid = id;
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7100/api/CarDescriptions?id=" + id);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData= await responseMessage.Content.ReadAsStringAsync();
+				var value = JsonConvert.DeserializeObject<ResultCarDescriptionWithCarIdDto>(jsonData);
+				return View(value);
+			}
+			
 			return View();
 		}
 	}
