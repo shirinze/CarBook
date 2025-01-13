0395d34c6c1a8e44486dea42d48c3ee469123b8e
shirinze <sherize1373@gmail.com>
2025-01-13 12:58:34 +0300

    register ve login sayfalari yazildi

---
+++ Core/CarBookApplication/Features/Mediator/Handlers/AppUserHandlers/CreateAppUserCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using CarBook.Domain.Entities;
+using CarBookApplication.Enums;
+using CarBookApplication.Features.Mediator.Commands.AppUserCommands;
+using CarBookApplication.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBookApplication.Features.Mediator.Handlers.AppUserHandlers
+{
+	public class CreateAppUserCommandHandler : IRequestHandler<CreateAppUserCommand>
+	{
+		private readonly IRepository<AppUser> _repository;
+
+		public CreateAppUserCommandHandler(IRepository<AppUser> repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task Handle(CreateAppUserCommand request, CancellationToken cancellationToken)
+		{
+			await _repository.CreateAsync(new AppUser 
+			{ 
+				UserName = request.Username, 
+				Password = request.Password ,
+				Email=request.Email,
+				Name=request.Name,
+				Surename=request.Surename,
+				AppRoleId=(int)RoleType.Member
+			});
+
+		}
+	}
+}
---
+++ Presentation/CarBook.WebApi/Controllers/RegistersController.cs
@@ -0,0 +1,25 @@
+﻿using CarBookApplication.Features.Mediator.Commands.AppUserCommands;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class RegistersController : ControllerBase
+	{
+		private readonly IMediator _mediator;
+
+		public RegistersController(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
+		[HttpPost]
+		public async Task<IActionResult> CreateUser(CreateAppUserCommand command)
+		{
+			await _mediator.Send(command);
+			return Ok("basarili bir sekilde eklendi");
+		}
+	}
+}
